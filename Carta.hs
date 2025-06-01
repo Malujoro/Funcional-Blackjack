169c3bf2397c446170f35f0b2e9ec341a571cd0a
Alef Cauan Sousa Rodriguês <alef.rodrigues@ufpi.edu.br>
2025-06-01 12:59:54 -0300

    feat: funções básicas  de jogador
    Co-authored-by: Mateus da Rocha Sousa <Malujoro@users.noreply.github.com>

--- Carta.hs
+++ Carta.hs
@@ -1,53 +1,77 @@
-module Carta (
-    Naipe(..),
-    Valor(..),
-    Carta(..),
+module Carta
+  ( Naipe (..),
+    Valor (..),
+    Carta (..),
     Mao,
-    Valoravel(..),
-) where
+    Valoravel (..),
+    todosValores,
+    todosNaipes
+  )
+where
 
 data Naipe = Copas | Espadas | Ouros | Paus
 
+todosNaipes :: [Naipe]
+todosNaipes = [Copas, Espadas, Ouros, Paus]
+
 data Valor = A | Dois | Tres | Quatro | Cinco | Seis | Sete | Oito | Nove | Dez | J | Q | K
+
+todosValores :: [Valor]
+todosValores = [A, Dois, Tres, Quatro, Cinco, Seis, Sete, Oito, Nove, Dez, J, Q, K]
 
 data Carta = Carta Valor Naipe
 
 type Mao = [Carta]
 
 instance Show Naipe where
-    show Copas = "♥"
-    show Espadas = "♠"
-    show Ouros = "♦"
-    show Paus = "♣"
+  show Copas = "♥"
+  show Espadas = "♠"
+  show Ouros = "♦"
+  show Paus = "♣"
 
 instance Show Valor where
-    show A = "A"
-    show Dois = "2"
-    show Tres = "3"
-    show Quatro = "4"
-    show Cinco = "5"
-    show Seis = "6"
-    show Sete = "7"
-    show Oito = "8"
-    show Nove = "9"
-    show Dez = "10"
-    show J = "J"
-    show Q = "Q"
-    show K = "K"
+  show A = "A"
+  show Dois = "2"
+  show Tres = "3"
+  show Quatro = "4"
+  show Cinco = "5"
+  show Seis = "6"
+  show Sete = "7"
+  show Oito = "8"
+  show Nove = "9"
+  show Dez = "10"
+  show J = "J"
+  show Q = "Q"
+  show K = "K"
 
 instance Show Carta where
-    show (Carta valor naipe) = "[" ++ show valor ++ " de " ++ show naipe ++ "]"
+  show (Carta valor naipe) = "[" ++ show valor ++ " de " ++ show naipe ++ "]"
 
 class Valoravel a where
-    valorNumerico :: a -> Int
+  valorNumerico :: a -> Int
 
 instance Valoravel Int where
-    valorNumerico n = n
+  valorNumerico n = n
 
 instance Valoravel Valor where
-    valorNumerico A = 1
-    valorNumerico J = 10
-    valorNumerico Q = 10
-    valorNumerico K = 10
-    valorNumerico val = read (show val) :: Int
+  valorNumerico A = 1
+  valorNumerico J = 10
+  valorNumerico Q = 10
+  valorNumerico K = 10
+  valorNumerico val = read (show val) :: Int
 
+instance Valoravel Carta where
+  valorNumerico (Carta valor _) = case valor of
+    A -> 11
+    Dois -> 2
+    Tres -> 3
+    Quatro -> 4
+    Cinco -> 5
+    Seis -> 6
+    Sete -> 7
+    Oito -> 8
+    Nove -> 9
+    Dez -> 10
+    J -> 10
+    Q -> 10
+    K -> 10
--- Main.hs
+++ Main.hs
@@ -3,7 +3,82 @@ module Main where
 import Carta
 import Jogador
 
+pegarCarta :: Jogador -> Carta -> Jogador
+pegarCarta jogador carta = jogador {mao = mao jogador ++ [carta]}
+
+saldoAtual :: [Int] -> Int
+saldoAtual = sum
+
+somarMao :: Mao -> Int
+somarMao mao = sum (map valorNumerico mao)
+
+data Resultado = Vitoria | Empate | Derrota deriving (Eq)
+
+data Situacao = VinteUm | Estouro | Incompleto deriving (Eq)
+
+verificarSituacao :: Mao -> Situacao
+verificarSituacao mao =
+  let total = somarMao mao
+   in if total > 21
+        then Estouro
+        else
+          if total == 21
+            then VinteUm
+            else Incompleto
+
+verificarVitoria :: Mao -> Mao -> Resultado
+verificarVitoria jogadorMao dealerMao =
+  let totalJogador = somarMao jogadorMao
+      totalDealer = somarMao dealerMao
+   in if totalJogador > totalDealer
+        then Vitoria
+        else
+          if totalJogador == totalDealer
+            then Empate
+            else Derrota
+
+verificarResultado :: Jogador -> Jogador -> Resultado
+verificarResultado jogador dealer =
+  let situacaoJogador = verificarSituacao (mao jogador)
+      situacaoDealer = verificarSituacao (mao dealer)
+   in if situacaoJogador == Incompleto && situacaoDealer == Incompleto
+        then
+          verificarVitoria (mao jogador) (mao dealer)
+        else
+          if situacaoJogador == situacaoDealer
+            then Empate
+            else
+              if situacaoJogador == VinteUm || situacaoDealer == Estouro
+                then Vitoria
+                -- situacaoDealer == VinteUm || situacaoJogador == Estouro
+                else Derrota
+
+addHistorico :: Jogador -> Int -> Jogador
+addHistorico (Jogador nome mao historico) valor =
+  Jogador nome mao (historico ++ [valor])
+
+limparBaralho :: Jogador -> Jogador
+limparBaralho (Jogador nome mao historico) =
+  Jogador nome [] historico
+
+criarBaralho :: Mao
+criarBaralho = [Carta valor naipe | valor <- todosValores, naipe <- todosNaipes]
+
 main :: IO ()
 main = do
-    let teste = Jogador "Cristina" [Carta Tres Ouros, Carta Seis Paus, Carta J Espadas] [2000, -500]
-    print (teste)
+  let player = Jogador "Cristina" [] []
+  let dealer = Jogador "Dealer" [] []
+
+  print player
+  print dealer
+
+  let new = addHistorico player 100
+  print new
+
+-- print teste
+-- let novoJogador = pegarCarta teste (Carta A Copas)
+-- print novoJogador
+-- let estourou = verificarEstouro novoJogador
+-- putStrLn $ if estourou
+--     then "O jogador estourou!"
+--     else "o jogador fez o L 🤣🤣🤣"
